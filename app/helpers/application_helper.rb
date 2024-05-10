5a2ff24e54ce8195e47915fd4efa02b48c456e14
juicyflash <tankedet@bk.ru>
2024-05-10 13:39:12 +0300

    2.2 UI вью стили

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,8 @@
 module ApplicationHelper
-  def nav_cart_name(cart)
-    "Cart #{cart.products_count}  total: #{cart.total}"
+  def nav_cart_price(cart)
+    cart.total
+  end
+  def nav_cart_products(cart)
+    "товаров: #{cart.products_count}"
   end
 end
