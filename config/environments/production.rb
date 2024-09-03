4cc4019f1f5eb5572abf3847a424dc1479231e13
JuicyFlash <tankedet@bk.ru>
2024-09-04 00:43:16 +0300

    localhost

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,8 @@ Rails.application.configure do
   # Don't log any deprecations.
   config.active_support.report_deprecations = false
 
+  config.action_mailer.default_url_options = { host: 'http://95.163.228.91'}
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
