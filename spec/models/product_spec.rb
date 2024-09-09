8a12f868bb423b91396095e98e8c8464ffdc7f18
JuicyFlash <tankedet@bk.ru>
2024-09-09 16:05:00 +0300

    Тесты Страница просмотра товаров

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,6 +5,7 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   it { should belong_to(:brand) }
   it { should have_many(:product_property).dependent(:destroy) }
+  it { should have_many(:properties) }
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:description) }
   it { should have_many_attached(:images) }
