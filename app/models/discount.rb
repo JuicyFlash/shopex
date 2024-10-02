4d126d959728a9e66ad0a1f3e5eba907ad49ea82
JuicyFlash <tankedet@bk.ru>
2024-10-02 16:46:21 +0300

    4.1 Тесты для моделей

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -1,4 +1,5 @@
 class Discount < ApplicationRecord
   has_many :conditions,  foreign_key: 'discount_id', class_name: 'DiscountCondition', dependent: :destroy
 
+  validates :value, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100 }
 end
--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Discount, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:conditions).dependent(:destroy) }
+
+  it { should validate_numericality_of(:value).is_greater_than_or_equal_to(0) }
+  it { should validate_numericality_of(:value).is_less_than_or_equal_to(100) }
 end
